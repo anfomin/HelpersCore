9bcfa4ee9c426ed5104452e75b8080e18a3782dc
Alexander Fomin <anfomin@me.com>
2026-04-01 14:55:04 +0300

    NavParams

---
+++ HelpersCore/NavParams.cs
@@ -0,0 +1,6 @@
+namespace HelpersCore;
+
+/// <summary>
+/// Represents dictionary for named navigation parameters.
+/// </summary>
+public class NavParams() : Dictionary<string, object?>(StringComparer.OrdinalIgnoreCase);
