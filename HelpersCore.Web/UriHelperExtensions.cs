b8422ef8be5c5be7fc4884f6944facc6d404cfcc
Alexander Fomin <anfomin@me.com>
2026-06-17 20:12:20 +0300

    Ability to combine relative URI with relative one

--- HelpersCore.Web/UriHelperExtensions.cs
+++ HelpersCore.Web/UriHelperExtensions.cs
@@ -11,17 +11,18 @@ public static class UriHelperExtensions
 	{
 		/// <summary>
 		/// Combines <paramref name="baseUri"/> and <paramref name="relativeUri"/> into one URI string.
+		/// If <paramref name="baseUri"/> doesn't end with '/', it will be considered as directory and '/' will be added before combining.
 		/// </summary>
-		/// <param name="baseUri">Base URL, may be absolute or relative.</param>
+		/// <param name="baseUri">Base URI, may be absolute or relative.</param>
 		/// <param name="relativeUri">Relative URI.</param>
 		/// <returns>Combined URI.</returns>
 		public static string Combine(string baseUri, string relativeUri)
 		{
 			if (relativeUri == string.Empty)
 				return baseUri;
-			Uri @base = new(baseUri.EndsWith('/') ? baseUri : $"{baseUri}/", UriKind.RelativeOrAbsolute);
-			string result = new Uri(@base, relativeUri).ToString();
-			return relativeUri.EndsWith('/') ? result : result.TrimEnd('/');
+			return new Uri(baseUri, UriKind.RelativeOrAbsolute)
+				.Combine(relativeUri, considerDirectory: true)
+				.ToString();
 		}
 	}
 }
--- HelpersCore/Extensions/UriExtensions.cs
+++ HelpersCore/Extensions/UriExtensions.cs
@@ -22,5 +22,75 @@ public static class UriExtensions
 		/// </summary>
 		public bool IsPathEndsWith(string suffix)
 			=> uri.AbsolutePath.EndsWith(suffix, StringComparison.OrdinalIgnoreCase);
+
+		/// <summary>
+		/// Combines current URI with <paramref name="relativeUri"/>.
+		/// </summary>
+		/// <param name="relativeUri">Relative URI to combine with.</param>
+		/// <param name="considerDirectory">
+		/// If <see langword="true"/> and current URI doesn't end with '/',
+		/// it will be considered as directory and '/' will be added before combining.
+		/// </param>
+		/// <returns>Combined URI.</returns>
+		public Uri Combine(string? relativeUri, bool considerDirectory = false)
+		{
+			if (string.IsNullOrEmpty(relativeUri))
+				return uri;
+			Uri relative = new(relativeUri, UriKind.Relative);
+			return uri.Combine(relative, considerDirectory);
+		}
+
+		/// <summary>
+		/// Combines current URI with <paramref name="relativeUri"/>.
+		/// </summary>
+		/// <param name="relativeUri">Relative URI to combine with.</param>
+		/// <param name="considerDirectory">
+		/// If <see langword="true"/> and current URI doesn't end with '/',
+		/// it will be considered as directory and '/' will be added before combining.
+		/// </param>
+		/// <returns>Combined URI.</returns>
+		public Uri Combine(Uri relativeUri, bool considerDirectory = false)
+		{
+			Uri? fakeBase = null;
+			if (!uri.IsAbsoluteUri)
+			{
+				fakeBase = new("http://localhost/");
+				uri = new(fakeBase, uri);
+			}
+
+			if (considerDirectory && !uri.AbsolutePath.EndsWith('/'))
+			{
+				var builder = new UriBuilder(uri);
+				builder.Path += '/';
+				uri = builder.Uri;
+			}
+
+			Uri combined = new(uri, relativeUri);
+			if (combined.AbsolutePath.EndsWith('/') && !Uri.GetWithoutQueryAndHash(relativeUri.OriginalString).EndsWith('/'))
+			{
+				var builder = new UriBuilder(combined);
+				builder.Path = builder.Path[..^1];
+				combined = builder.Uri;
+			}
+			if (fakeBase != null)
+				combined = fakeBase.MakeRelativeUri(combined);
+			return combined;
+		}
+	}
+
+	extension(Uri)
+	{
+		/// <summary>
+		/// Returns URI without query string and hash part.
+		/// </summary>
+		/// <param name="uri">URI to parse.</param>
+		/// <returns>URI without query string and hash part</returns>
+		public static ReadOnlySpan<char> GetWithoutQueryAndHash(ReadOnlySpan<char> uri)
+		{
+			int index = uri.IndexOf('?');
+			if (index == -1)
+				index = uri.IndexOf('#');
+			return index == -1 ? uri : uri[..index];
+		}
 	}
 }
