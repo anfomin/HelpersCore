c1c65a5bfded695d448e43991264bc4affba2ba0
Alexander Fomin <anfomin@me.com>
2026-08-06 18:10:23 +0300

    RenderComponentToHtmlAsync with Type argument

--- HelpersCore.Web/HtmlRendererExtensions.cs
+++ HelpersCore.Web/HtmlRendererExtensions.cs
@@ -13,6 +13,20 @@ public static class HtmlRendererExtensions
 		/// <summary>
 		/// Creates an instance of the specified component and renders it to HTML using renderer dispatcher.
 		/// </summary>
+		/// <param name="componentType">The component type.</param>
+		/// <param name="parameters">Parameters for the component.</param>
+		/// <returns>HTML string.</returns>
+		public Task<string> RenderComponentToHtmlAsync(Type componentType, Dictionary<string, object?>? parameters = null)
+			=> htmlRenderer.Dispatcher.InvokeAsync(async () =>
+			{
+				var parameterView = parameters is null ? ParameterView.Empty : ParameterView.FromDictionary(parameters);
+				var root = await htmlRenderer.RenderComponentAsync(componentType, parameterView);
+				return root.ToHtmlString();
+			});
+
+		/// <summary>
+		/// Creates an instance of the specified component and renders it to HTML using renderer dispatcher.
+		/// </summary>
 		/// <typeparam name="TComponent">The component type.</typeparam>
 		/// <param name="parameters">Parameters for the component.</param>
 		/// <returns>HTML string.</returns>
@@ -23,6 +37,20 @@ public static class HtmlRendererExtensions
 				var parameterView = parameters is null ? ParameterView.Empty : ParameterView.FromDictionary(parameters);
 				var root = await htmlRenderer.RenderComponentAsync<TComponent>(parameterView);
 				return root.ToHtmlString();
+			});
+
+		/// <summary>
+		/// Creates an instance of the specified component and renders it to HTML using renderer dispatcher.
+		/// </summary>
+		/// <param name="componentType">The component type.</param>
+		/// <param name="output">The output destination.</param>
+		/// <param name="parameters">Parameters for the component.</param>
+		public Task RenderComponentToHtmlAsync(Type componentType, TextWriter output, Dictionary<string, object?>? parameters = null)
+			=> htmlRenderer.Dispatcher.InvokeAsync(async () =>
+			{
+				var parameterView = parameters is null ? ParameterView.Empty : ParameterView.FromDictionary(parameters);
+				var root = await htmlRenderer.RenderComponentAsync(componentType, parameterView);
+				root.WriteHtmlTo(output);
 			});
 
 		/// <summary>
